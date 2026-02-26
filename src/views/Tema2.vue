f98eb4de9fdebc27179b2ed9236556e1e2467f0d
Andrea Botello <andrea.botellor@misena.edu.co>
2026-02-26 11:57:38 -0500

    Entrega [2.1]

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,19 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Tema 2
+      h1 Asertividad en la comunicación
+
+
+    .row.justify-content-center.mb-5
+      .col-lg-6.col-md-8.mb-3.mb-lg-0
+        img(src='@/assets/curso/temas/t2/1.png', alt='')
+      .col-lg-6  
+        p En ambientes laborales es frecuente enfrentarse a situaciones complejas en las que surgen dudas sobre: qué decir, cómo decirlo, cuándo hacerlo y si es conveniente expresar lo que se piensa o se siente. Todas estas decisiones comunicativas influyen directamente en las relaciones de trabajo, el clima organizacional y la resolución de conflictos.
+        .cajon.color-primario.p-4.mb-4
+          p.mb-0 Es por esta razón que, la asertividad es una forma de comunicación que permite expresar pensamientos, emociones, necesidades y opiniones de manera clara, directa y oportuna, sin vulnerar los derechos de los demás ni renunciar a los propios. 
+
+
+
 
 </template>
 
